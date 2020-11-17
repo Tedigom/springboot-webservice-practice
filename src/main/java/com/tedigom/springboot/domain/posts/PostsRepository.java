c44d1098452e4d0cc74abe01feacf5e1137049be
sehwan <tedigom7@gmail.com>
2020-11-17 21:55:24 +0900

    add mustache component

--- src/main/java/com/tedigom/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/tedigom/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.tedigom.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // JpaRepository<Entity 클래스, PK 타입> 선언
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/tedigom/springboot/service/posts/PostsService.java
+++ src/main/java/com/tedigom/springboot/service/posts/PostsService.java
@@ -3,6 +3,7 @@ package com.tedigom.springboot.service.posts;
 
 import com.tedigom.springboot.domain.posts.Posts;
 import com.tedigom.springboot.domain.posts.PostsRepository;
+import com.tedigom.springboot.web.dto.PostsListResponseDto;
 import com.tedigom.springboot.web.dto.PostsResponseDto;
 import com.tedigom.springboot.web.dto.PostsSaveRequestDto;
 import com.tedigom.springboot.web.dto.PostsUpdateRequestDto;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,12 @@ public class PostsService {
                 "해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
