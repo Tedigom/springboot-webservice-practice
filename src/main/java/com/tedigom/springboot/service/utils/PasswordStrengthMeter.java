ee8d016f614e0eddbf990b2ff5b570d3078d9fba
sehwanKim <tedigom7@gmail.com>
2023-06-08 16:24:38 +0900

    Junit test study

--- src/main/java/com/tedigom/springboot/service/utils/PasswordStrengthMeter.java
+++ src/main/java/com/tedigom/springboot/service/utils/PasswordStrengthMeter.java
@@ -6,17 +6,33 @@ package com.tedigom.springboot.service.utils;
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
-        if (s.length()<8){
-            return PasswordStrength.NORMAL;
-        }
-        boolean containsNum = meetsContainingNumberCriteria(s);
-        if (!containsNum) return PasswordStrength.NORMAL;
+        int metCounts = getMetCriteriaCounts(s);
+        if (metCounts <= 1)return PasswordStrength.WEAK;
+        if (metCounts == 2)return PasswordStrength.NORMAL; // 이전의 if 구문을 하나로 제거하였다.
+
         return PasswordStrength.STRONG;
+    }
+
+    private int getMetCriteriaCounts(String s) {
+        int metCounts = 0;
+        if (s.length() >= 8) metCounts++;
+        if (meetsContainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUpperCaseCriteria(s)) metCounts++;
+        return metCounts;
     }
 
     private boolean meetsContainingNumberCriteria(String s){
         for (char ch : s.toCharArray()){
             if (ch >= '0' && ch <= '9'){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean meetsContainingUpperCaseCriteria(String s){
+        for (char ch : s.toCharArray()){
+            if (Character.isUpperCase(ch)){
                 return true;
             }
         }
--- src/test/java/com/tedigom/springboot/web/PasswordStrengthMeterTest.java
+++ src/test/java/com/tedigom/springboot/web/PasswordStrengthMeterTest.java
@@ -15,16 +15,47 @@ import static org.junit.Assert.assertEquals;
 public class PasswordStrengthMeterTest {
     PasswordStrengthMeter meter = new PasswordStrengthMeter();
 
+    // 기본 테스트
     @Test
     public void meetsAllCriteria_Then_Strong(){
         assertStrength("ab12!@AB", PasswordStrength.STRONG);
         assertStrength("abc1!Add", PasswordStrength.STRONG);
     }
 
+    // null 값 혹은 empty 값에 대한 test
     @Test
     public void nullInput_Then_Invalid() {
         assertStrength(null, PasswordStrength.INVALID);
         assertStrength("", PasswordStrength.INVALID);
+    }
+
+    // Uppercase가 들어가지 않은 경우에 대한 test
+    @Test
+    public void meetsOtherCriteria_except_for_Uppercase_Then_Normal() {
+        assertStrength("ab12!@df", PasswordStrength.NORMAL);
+    }
+
+    // 길이가 8글자 이상인 조건만 충족하는 경우에 대한 test
+    @Test
+    public void meetsOnlyLengthCriteria_Then_Weak() {
+        assertStrength("abdefghi", PasswordStrength.WEAK);
+    }
+
+    // 숫자 포함 조건만 충족하는 경우에 대한 test
+    @Test
+    public void meetsOnlyNumCriteria_Then_Weak() {
+        assertStrength("12345", PasswordStrength.WEAK);
+    }
+
+    // 대문자 포함 조건만 충족하는 경우 test
+    @Test
+    public void meetsOnlyUpperCriteria_Then_Weak() {
+        assertStrength("ABZEF", PasswordStrength.WEAK);
+    }
+
+    @Test
+    public void meetsNoCriteria_Then_Weak() {
+        assertStrength("abc", PasswordStrength.WEAK);
     }
     private void assertStrength(String password, PasswordStrength expStr) {
         PasswordStrength result = meter.meter(password);
